237b86cd28db4be3a454bb428f1e7a7e674ccb72
JOGOH MYKAEL <michaeljogoh>
2022-08-05 16:30:11 +0100

    Url Model

--- app.js
+++ app.js
@@ -4,11 +4,18 @@ require('dotenv').config();
 const PORT = process.env.PORT;
 const mongoose = require('mongoose');
 
+// Connect MongoDB
+const  connectDB = async () =>{
+ await mongoose.connect(process.env.URl_DB,{useNewUrlParser: true , useUnifiedTopology: true})
+  console.log('MongoDB')
+}
+connectDB();
 
 
 
 
 
+//Port
 app.listen(PORT , ()=>{
     console.log(`Server started at port ${PORT}`)
 })
--- models/Url.js
+++ models/Url.js
@@ -1,9 +1,15 @@
 const mongoose = require('mongoose');
+const shortid = require('shortid');
 
 const UrlSchema =  new mongoose.Schema({
     fullurl:{
         type: String,
         required: true
+    },
+    
+    shorturl:{
+        type:String,
+        default: shortid.generate
     }
 
 })
